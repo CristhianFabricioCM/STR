e8d69f0dec797cc887b9de2fcac856c48b894447
juans.cespedes <juans.cespedes@LSTKLM290470>
2024-03-24 02:10:25 -0500

    Cambios de vista

--- Sol_PuntoVenta.Presentacion/Frm_login.Designer.cs
+++ Sol_PuntoVenta.Presentacion/Frm_login.Designer.cs
@@ -57,9 +57,8 @@ namespace Sol_PuntoVenta.Presentacion
             this.panel1.Controls.Add(this.label4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(333, 406);
+            this.panel1.Size = new System.Drawing.Size(250, 330);
             this.panel1.TabIndex = 0;
             this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
@@ -67,10 +66,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.label5.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(0, 370);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(0, 301);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(333, 36);
+            this.label5.Size = new System.Drawing.Size(250, 29);
             this.label5.TabIndex = 2;
             this.label5.Text = "Sistemas en Tiempo Real";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -78,10 +76,9 @@ namespace Sol_PuntoVenta.Presentacion
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(79, 22);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(59, 18);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(172, 199);
+            this.pictureBox1.Size = new System.Drawing.Size(129, 162);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -90,22 +87,20 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.label4.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(4, 217);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(3, 176);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(325, 154);
+            this.label4.Size = new System.Drawing.Size(244, 125);
             this.label4.TabIndex = 0;
-            this.label4.Text = "Sistema Gestión Restaurant";
+            this.label4.Text = "Sistema de Gestión\r\nde Restaurante";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Txt_email_us
             // 
             this.Txt_email_us.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Txt_email_us.Location = new System.Drawing.Point(451, 116);
-            this.Txt_email_us.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_email_us.Location = new System.Drawing.Point(338, 94);
             this.Txt_email_us.MaxLength = 150;
             this.Txt_email_us.Name = "Txt_email_us";
-            this.Txt_email_us.Size = new System.Drawing.Size(412, 27);
+            this.Txt_email_us.Size = new System.Drawing.Size(310, 23);
             this.Txt_email_us.TabIndex = 1;
             this.Txt_email_us.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_email_us_KeyPress);
             // 
@@ -113,10 +108,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(451, 95);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(338, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(146, 18);
+            this.label1.Size = new System.Drawing.Size(121, 14);
             this.label1.TabIndex = 2;
             this.label1.Text = "Email y/o Usuario:";
             // 
@@ -124,22 +118,20 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(451, 170);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(338, 138);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 18);
+            this.label2.Size = new System.Drawing.Size(86, 14);
             this.label2.TabIndex = 4;
             this.label2.Text = "Contraseña:";
             // 
             // Txt_password_us
             // 
             this.Txt_password_us.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Txt_password_us.Location = new System.Drawing.Point(451, 193);
-            this.Txt_password_us.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Txt_password_us.Location = new System.Drawing.Point(338, 157);
             this.Txt_password_us.MaxLength = 50;
             this.Txt_password_us.Name = "Txt_password_us";
             this.Txt_password_us.PasswordChar = '*';
-            this.Txt_password_us.Size = new System.Drawing.Size(412, 27);
+            this.Txt_password_us.Size = new System.Drawing.Size(310, 23);
             this.Txt_password_us.TabIndex = 3;
             this.Txt_password_us.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Txt_password_us_KeyPress);
             // 
@@ -147,10 +139,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(600, 11);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(450, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(154, 29);
+            this.label3.Size = new System.Drawing.Size(119, 24);
             this.label3.TabIndex = 5;
             this.label3.Text = "Iniciar sesión";
             // 
@@ -166,10 +157,9 @@ namespace Sol_PuntoVenta.Presentacion
             this.Btn_acceder.IconChar = FontAwesome.Sharp.IconChar.None;
             this.Btn_acceder.IconColor = System.Drawing.Color.Black;
             this.Btn_acceder.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.Btn_acceder.Location = new System.Drawing.Point(451, 267);
-            this.Btn_acceder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Btn_acceder.Location = new System.Drawing.Point(338, 217);
             this.Btn_acceder.Name = "Btn_acceder";
-            this.Btn_acceder.Size = new System.Drawing.Size(413, 49);
+            this.Btn_acceder.Size = new System.Drawing.Size(310, 40);
             this.Btn_acceder.TabIndex = 0;
             this.Btn_acceder.Text = "ACCEDER";
             this.Btn_acceder.UseVisualStyleBackColor = false;
@@ -180,10 +170,9 @@ namespace Sol_PuntoVenta.Presentacion
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.linkLabel1.LinkColor = System.Drawing.Color.DimGray;
-            this.linkLabel1.Location = new System.Drawing.Point(525, 354);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(394, 288);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(227, 18);
+            this.linkLabel1.Size = new System.Drawing.Size(194, 14);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "¿Has olvidado tu contraseña?";
@@ -194,10 +183,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.Pct_minimizar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Pct_minimizar.Image = ((System.Drawing.Image)(resources.GetObject("Pct_minimizar.Image")));
-            this.Pct_minimizar.Location = new System.Drawing.Point(961, 11);
-            this.Pct_minimizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Pct_minimizar.Location = new System.Drawing.Point(721, 9);
             this.Pct_minimizar.Name = "Pct_minimizar";
-            this.Pct_minimizar.Size = new System.Drawing.Size(24, 22);
+            this.Pct_minimizar.Size = new System.Drawing.Size(18, 18);
             this.Pct_minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Pct_minimizar.TabIndex = 8;
             this.Pct_minimizar.TabStop = false;
@@ -207,10 +195,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             this.Pct_cerrar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Pct_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("Pct_cerrar.Image")));
-            this.Pct_cerrar.Location = new System.Drawing.Point(1000, 11);
-            this.Pct_cerrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Pct_cerrar.Location = new System.Drawing.Point(750, 9);
             this.Pct_cerrar.Name = "Pct_cerrar";
-            this.Pct_cerrar.Size = new System.Drawing.Size(24, 22);
+            this.Pct_cerrar.Size = new System.Drawing.Size(18, 18);
             this.Pct_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.Pct_cerrar.TabIndex = 7;
             this.Pct_cerrar.TabStop = false;
@@ -218,9 +205,9 @@ namespace Sol_PuntoVenta.Presentacion
             // 
             // Frm_login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1040, 406);
+            this.ClientSize = new System.Drawing.Size(780, 330);
             this.Controls.Add(this.Pct_minimizar);
             this.Controls.Add(this.Pct_cerrar);
             this.Controls.Add(this.linkLabel1);
@@ -233,7 +220,6 @@ namespace Sol_PuntoVenta.Presentacion
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Frm_login";
             this.Opacity = 0.9D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
